6e49666b21298a852d37bbf048963fb5169c6fc0
Michael de Wit <mjwwit@gmail.com>
2019-03-08 10:51:25 +0100

    Change enum API to direct input of enum values Allow up to 9 enum values with TypeScript support

--- src/enum.ts
+++ src/enum.ts
@@ -1,100 +1,148 @@
-import { BaseSchemaDefinition, TypedSchema } from './common'
+import { TypedSchema } from './common'
 
 type LiteralValue = string | number | boolean | null
 
-interface EnumSchemaDefinition1<V1 extends LiteralValue>
-  extends BaseSchemaDefinition {
-  /**
-   * A fixed set of allowed values
-   */
-  enum: [V1]
-}
-interface EnumSchemaDefinition2<
-  V1 extends LiteralValue,
-  V2 extends LiteralValue
-> extends BaseSchemaDefinition {
-  /**
-   * A fixed set of allowed values
-   */
-  enum: [V1, V2]
-}
-interface EnumSchemaDefinition3<
+type Enum1<V1 extends LiteralValue> = [V1]
+type Enum2<V1 extends LiteralValue, V2 extends LiteralValue> = [V1, V2]
+type Enum3<
   V1 extends LiteralValue,
   V2 extends LiteralValue,
   V3 extends LiteralValue
-> extends BaseSchemaDefinition {
-  /**
-   * A fixed set of allowed values
-   */
-  enum: [V1, V2, V3]
-}
-interface EnumSchemaDefinition4<
+> = [V1, V2, V3]
+type Enum4<
   V1 extends LiteralValue,
   V2 extends LiteralValue,
   V3 extends LiteralValue,
   V4 extends LiteralValue
-> extends BaseSchemaDefinition {
-  /**
-   * A fixed set of allowed values
-   */
-  enum: [V1, V2, V3, V4]
-}
-interface EnumSchemaDefinition5<
+> = [V1, V2, V3, V4]
+type Enum5<
   V1 extends LiteralValue,
   V2 extends LiteralValue,
   V3 extends LiteralValue,
   V4 extends LiteralValue,
   V5 extends LiteralValue
-> extends BaseSchemaDefinition {
-  /**
-   * A fixed set of allowed values
-   */
-  enum: [V1, V2, V3, V4, V5]
-}
-interface EnumSchemaDefinitionRest extends BaseSchemaDefinition {
-  /**
-   * A fixed set of allowed values
-   */
-  enum: LiteralValue[]
-}
+> = [V1, V2, V3, V4, V5]
+type Enum6<
+  V1 extends LiteralValue,
+  V2 extends LiteralValue,
+  V3 extends LiteralValue,
+  V4 extends LiteralValue,
+  V5 extends LiteralValue,
+  V6 extends LiteralValue
+> = [V1, V2, V3, V4, V5, V6]
+type Enum7<
+  V1 extends LiteralValue,
+  V2 extends LiteralValue,
+  V3 extends LiteralValue,
+  V4 extends LiteralValue,
+  V5 extends LiteralValue,
+  V6 extends LiteralValue,
+  V7 extends LiteralValue
+> = [V1, V2, V3, V4, V5, V6, V7]
+type Enum8<
+  V1 extends LiteralValue,
+  V2 extends LiteralValue,
+  V3 extends LiteralValue,
+  V4 extends LiteralValue,
+  V5 extends LiteralValue,
+  V6 extends LiteralValue,
+  V7 extends LiteralValue,
+  V8 extends LiteralValue
+> = [V1, V2, V3, V4, V5, V6, V7, V8]
+type Enum9<
+  V1 extends LiteralValue,
+  V2 extends LiteralValue,
+  V3 extends LiteralValue,
+  V4 extends LiteralValue,
+  V5 extends LiteralValue,
+  V6 extends LiteralValue,
+  V7 extends LiteralValue,
+  V8 extends LiteralValue,
+  V9 extends LiteralValue
+> = [V1, V2, V3, V4, V5, V6, V7, V8, V9]
+type EnumRest = LiteralValue[]
 
-function enumType<V1 extends LiteralValue>(
-  schema: EnumSchemaDefinition1<V1>
-): TypedSchema<V1>
+function enumType<V1 extends LiteralValue>(values: Enum1<V1>): TypedSchema<V1>
 function enumType<V1 extends LiteralValue, V2 extends LiteralValue>(
-  schema: EnumSchemaDefinition2<V1, V2>
+  values: Enum2<V1, V2>
 ): TypedSchema<V1 | V2>
 function enumType<
   V1 extends LiteralValue,
   V2 extends LiteralValue,
   V3 extends LiteralValue
->(schema: EnumSchemaDefinition3<V1, V2, V3>): TypedSchema<V1 | V2 | V3>
+>(values: Enum3<V1, V2, V3>): TypedSchema<V1 | V2 | V3>
 function enumType<
   V1 extends LiteralValue,
   V2 extends LiteralValue,
   V3 extends LiteralValue,
   V4 extends LiteralValue
->(schema: EnumSchemaDefinition4<V1, V2, V3, V4>): TypedSchema<V1 | V2 | V3 | V4>
+>(values: Enum4<V1, V2, V3, V4>): TypedSchema<V1 | V2 | V3 | V4>
 function enumType<
   V1 extends LiteralValue,
   V2 extends LiteralValue,
   V3 extends LiteralValue,
   V4 extends LiteralValue,
   V5 extends LiteralValue
+>(values: Enum5<V1, V2, V3, V4, V5>): TypedSchema<V1 | V2 | V3 | V4 | V5>
+function enumType<
+  V1 extends LiteralValue,
+  V2 extends LiteralValue,
+  V3 extends LiteralValue,
+  V4 extends LiteralValue,
+  V5 extends LiteralValue,
+  V6 extends LiteralValue
 >(
-  schema: EnumSchemaDefinition5<V1, V2, V3, V4, V5>
-): TypedSchema<V1 | V2 | V3 | V4 | V5>
-function enumType(schema: EnumSchemaDefinitionRest): TypedSchema<LiteralValue> {
+  values: Enum6<V1, V2, V3, V4, V5, V6>
+): TypedSchema<V1 | V2 | V3 | V4 | V5 | V6>
+function enumType<
+  V1 extends LiteralValue,
+  V2 extends LiteralValue,
+  V3 extends LiteralValue,
+  V4 extends LiteralValue,
+  V5 extends LiteralValue,
+  V6 extends LiteralValue,
+  V7 extends LiteralValue
+>(
+  values: Enum7<V1, V2, V3, V4, V5, V6, V7>
+): TypedSchema<V1 | V2 | V3 | V4 | V5 | V6 | V7>
+function enumType<
+  V1 extends LiteralValue,
+  V2 extends LiteralValue,
+  V3 extends LiteralValue,
+  V4 extends LiteralValue,
+  V5 extends LiteralValue,
+  V6 extends LiteralValue,
+  V7 extends LiteralValue,
+  V8 extends LiteralValue
+>(
+  values: Enum8<V1, V2, V3, V4, V5, V6, V7, V8>
+): TypedSchema<V1 | V2 | V3 | V4 | V5 | V6 | V7 | V8>
+function enumType<
+  V1 extends LiteralValue,
+  V2 extends LiteralValue,
+  V3 extends LiteralValue,
+  V4 extends LiteralValue,
+  V5 extends LiteralValue,
+  V6 extends LiteralValue,
+  V7 extends LiteralValue,
+  V8 extends LiteralValue,
+  V9 extends LiteralValue
+>(
+  values: Enum9<V1, V2, V3, V4, V5, V6, V7, V8, V9>
+): TypedSchema<V1 | V2 | V3 | V4 | V5 | V6 | V7 | V8 | V9>
+function enumType(values: EnumRest): TypedSchema<LiteralValue> {
   return {
-    getSchema: () => Object.assign({}, schema),
+    getSchema: () => Object.assign({}, { enum: values }),
     type: 'ENUM_VALUE' as any,
   }
 }
 
-function constType<T extends LiteralValue>(value: T): TypedSchema<T> {
+function constType<Value extends LiteralValue>(
+  value: Value
+): TypedSchema<Value> {
   return {
     getSchema: () => ({ const: value }),
-    type: 'CONST_VALUE' as T,
+    type: 'CONST_VALUE' as Value,
   }
 }
 
--- test/enum.spec.ts
+++ test/enum.spec.ts
@@ -2,17 +2,13 @@ import test from 'tape'
 import * as s from '../src/tson-schema'
 
 test('Enum type schema', (t) => {
-  const enumSchema = s.enum({
-    enum: ['A', 2],
-  })
+  const enumSchema = s.enum(['A', 2])
   t.deepEquals(enumSchema.getSchema(), { enum: ['A', 2] })
 
   const x: typeof enumSchema.type = 'A'
   const y: typeof enumSchema.type = 2
 
-  const enum5Schema = s.enum({
-    enum: ['A', 'B', 'C', 'D', 'E'],
-  })
+  const enum5Schema = s.enum(['A', 'B', 'C', 'D', 'E'])
   t.deepEquals(enum5Schema.getSchema(), { enum: ['A', 'B', 'C', 'D', 'E'] })
 
   const z: typeof enum5Schema.type = 'E'
