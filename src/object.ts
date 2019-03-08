dc9676027d0311d9923166669533f47770cfe00e
Michael de Wit <mjwwit@gmail.com>
2019-03-08 10:57:46 +0100

    Allow up to 9 required properties for an object type with TS support

--- src/object.ts
+++ src/object.ts
@@ -54,6 +54,60 @@ export interface ObjectDefinition5<
   properties: { [Key in keyof T]: TypedSchema<T[Key]> }
   required: [R1, R2, R3, R4, R5]
 }
+export interface ObjectDefinition6<
+  T extends {},
+  R1 extends keyof T,
+  R2 extends keyof T,
+  R3 extends keyof T,
+  R4 extends keyof T,
+  R5 extends keyof T,
+  R6 extends keyof T
+> extends BaseSchemaDefinition {
+  properties: { [Key in keyof T]: TypedSchema<T[Key]> }
+  required: [R1, R2, R3, R4, R5, R6]
+}
+export interface ObjectDefinition7<
+  T extends {},
+  R1 extends keyof T,
+  R2 extends keyof T,
+  R3 extends keyof T,
+  R4 extends keyof T,
+  R5 extends keyof T,
+  R6 extends keyof T,
+  R7 extends keyof T
+> extends BaseSchemaDefinition {
+  properties: { [Key in keyof T]: TypedSchema<T[Key]> }
+  required: [R1, R2, R3, R4, R5, R6, R7]
+}
+export interface ObjectDefinition8<
+  T extends {},
+  R1 extends keyof T,
+  R2 extends keyof T,
+  R3 extends keyof T,
+  R4 extends keyof T,
+  R5 extends keyof T,
+  R6 extends keyof T,
+  R7 extends keyof T,
+  R8 extends keyof T
+> extends BaseSchemaDefinition {
+  properties: { [Key in keyof T]: TypedSchema<T[Key]> }
+  required: [R1, R2, R3, R4, R5, R6, R7, R8]
+}
+export interface ObjectDefinition9<
+  T extends {},
+  R1 extends keyof T,
+  R2 extends keyof T,
+  R3 extends keyof T,
+  R4 extends keyof T,
+  R5 extends keyof T,
+  R6 extends keyof T,
+  R7 extends keyof T,
+  R8 extends keyof T,
+  R9 extends keyof T
+> extends BaseSchemaDefinition {
+  properties: { [Key in keyof T]: TypedSchema<T[Key]> }
+  required: [R1, R2, R3, R4, R5, R6, R7, R8, R9]
+}
 
 function objectType<T>(
   schema: PartialObjectDefinition<T>
@@ -91,6 +145,62 @@ function objectType<
 >(
   schema: ObjectDefinition5<T, R1, R2, R3, R4, R5>
 ): TypedSchema<Partial<T> & { [key in R1 | R2 | R3 | R4 | R5]: T[key] }>
+function objectType<
+  T,
+  R1 extends keyof T,
+  R2 extends keyof T,
+  R3 extends keyof T,
+  R4 extends keyof T,
+  R5 extends keyof T,
+  R6 extends keyof T
+>(
+  schema: ObjectDefinition6<T, R1, R2, R3, R4, R5, R6>
+): TypedSchema<Partial<T> & { [key in R1 | R2 | R3 | R4 | R5 | R6]: T[key] }>
+function objectType<
+  T,
+  R1 extends keyof T,
+  R2 extends keyof T,
+  R3 extends keyof T,
+  R4 extends keyof T,
+  R5 extends keyof T,
+  R6 extends keyof T,
+  R7 extends keyof T
+>(
+  schema: ObjectDefinition7<T, R1, R2, R3, R4, R5, R6, R7>
+): TypedSchema<
+  Partial<T> & { [key in R1 | R2 | R3 | R4 | R5 | R6 | R7]: T[key] }
+>
+function objectType<
+  T,
+  R1 extends keyof T,
+  R2 extends keyof T,
+  R3 extends keyof T,
+  R4 extends keyof T,
+  R5 extends keyof T,
+  R6 extends keyof T,
+  R7 extends keyof T,
+  R8 extends keyof T
+>(
+  schema: ObjectDefinition8<T, R1, R2, R3, R4, R5, R6, R7, R8>
+): TypedSchema<
+  Partial<T> & { [key in R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8]: T[key] }
+>
+function objectType<
+  T,
+  R1 extends keyof T,
+  R2 extends keyof T,
+  R3 extends keyof T,
+  R4 extends keyof T,
+  R5 extends keyof T,
+  R6 extends keyof T,
+  R7 extends keyof T,
+  R8 extends keyof T,
+  R9 extends keyof T
+>(
+  schema: ObjectDefinition9<T, R1, R2, R3, R4, R5, R6, R7, R8, R9>
+): TypedSchema<
+  Partial<T> & { [key in R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9]: T[key] }
+>
 function objectType<T>(schema: ObjectDefinition<T>): TypedSchema<T> {
   return {
     getSchema: () =>
